c27fad9c6c3e491d3cab507a476deeb9f6462a12
Linda Goldstein <linda.goldstein@parachutehealth.com>
2022-12-06 15:26:02 -0600

    5a

--- 2022/5.rb
+++ 2022/5.rb
@@ -10,11 +10,28 @@
 # move 2 from 2 to 1
 # move 1 from 1 to 2
 
+
+#             [G] [W]         [Q]
+# [Z]         [Q] [M]     [J] [F]
+# [V]         [V] [S] [F] [N] [R]
+# [T]         [F] [C] [H] [F] [W] [P]
+# [B] [L]     [L] [J] [C] [V] [D] [V]
+# [J] [V] [F] [N] [T] [T] [C] [Z] [W]
+# [G] [R] [Q] [H] [Q] [W] [Z] [G] [B]
+# [R] [J] [S] [Z] [R] [S] [D] [L] [J]
+#  1   2   3   4   5   6   7   8   9
+
 stacks = [
   [], # pretend to be 0-indexed thanks
-  %w[z n],
-  %w[m c d],
-  %w[p],
+  %w[r g j b t v z],
+  %w[j r v l],
+  %w[s q f],
+  %w[z h n l f v q g],
+  %w[r q t j c s m w],
+  %w[s w t c h f],
+  %w[d z c v f n j],
+  %w[l g z d w r f q],
+  %w[j b w v p],
 ]
 
 require 'pry'
