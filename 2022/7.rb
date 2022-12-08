ec32294631d15b35ddde7322962ac643538d7364
Linda Goldstein <linda.goldstein@parachutehealth.com>
2022-12-07 23:00:27 -0600

    something 7

--- 2022/7.rb
+++ 2022/7.rb
@@ -39,98 +39,32 @@ end
 
 sizes = Hash.new(0)
 
-def foo(item, total = 0)
+def foo(item, current_key = "", totals = Hash.new(0))
+  # p "foo #{current_key}"
+  # p totals
+  # p item
   if item.class == Hash
     if item.has_key?(:filesize)
-      total += item[:filesize].to_i
-      p "new total #{total}"
+      totals[current_key] += item[:filesize].to_i
+      totals
     else
       item.keys.each do |k|
-        foo(item[k], total)
+        foo(item[k], current_key += k, totals)
       end
     end
   elsif item.class == Array
     item.each do |i2|
-      foo(i2, total)
+      foo(i2, current_key, totals)
     end
   end
+  totals
 end
 
-tree.keys.each do |k|
-  tree[k].each do |item|
-    binding.pry
-    if item.class == Hash
-      if item.has_key?(:filesize)
-      else
-        item.keys.each do |k2|
-          if item[k2].class == Array
-            item[k2].each do |item2|
-            end
-          end
-        end
-      end
-    elsif item.class == Array
-    end
-  end
-  item.keys.each do |k2|
-    item2 = item[k2]
-    if item.class == Hash && item.has_key?(:filesize)
-      filesize = item[:filesize].to_i
-      sizes[k] += filesize
-      sizes["#{k}#{item}"] += filesize
-      sizes["#{k}#{item}#{item2}"] += filesize
-    end
-  end
-end
+result = tree.keys.map do |k|
+  foo(tree[k])
 end
 
-p tree
-# binding.pry
-bigs = []
-
-# def sum_r(dir, dir_sums = {})
-#   p "sum_r"
-#   dir.each do |sub_d|
-#     p [sub_d.class]
-#     if sub_d.class == Array
-#       p "arr"
-#       sum_r(sub_d)
-#     elsif sub_d.class == Hash
-#       if sub_d.has_key?(:filesize)
-#         p sub_d
-#         dir_sums[sub_d] ||= 0
-#         dir_sums[sub_d] += sub_d[:filesize].to_i
-#       else
-#         sub_d.values.each { |ss|
-#           sum_r(ss)
-#         }
-#       end
-#     end
-#   end
-#   dir_sums
-# end
-#
-# p sum_r(tree)
-
-# for every key in the tree,
-
-# binding.pry
-#
-# def sum_r(x)
-#   return x.first if x.length == 1 && x.first.first != "\\"
-#   return 0 if x.length < 1
-#
-#   x.to_a.pop + sum_r(x.to_a)
-# end
-# sum_r(tree)
-#
-#
-# p tree.to_a.flatten
-#   .map { |f|
-#     f.class == Hash && f.has_key?(:filesize) ? f[:filesize] : 0
-#   }
-#   .inject(:+)
-
-
+p result
+# [{"ae"=>584, "a"=>94269, ""=>23352670, "d"=>24933642}]
 
 
