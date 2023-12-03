a0309834dd9f571ac61ac778007e278d4a965a0a
compwron <compwron@users.noreply.github.com>
2023-12-03 10:11:25 -0800

    part 2 day 2

--- 2023/2.py
+++ 2023/2.py
@@ -22,3 +22,26 @@ for line in input.splitlines():
         if (count > maximums[color]):
           valid_game_ids.remove(gameid)
 answer = print(sum(valid_game_ids))
+
+
+
+input = open("input.txt", "r").read()
+# print(input) 
+
+sum = 0
+
+for line in input.splitlines():
+  parts = line.split(": ")
+  gameid = int(parts[0][5:])
+  maximums = {}
+  for group in parts[1].split('; '):
+    for subgroup in group.split(', '):
+      subparts = subgroup.split(' ')
+      count = int(subparts[0])
+      color = subparts[1]
+      maxcolor = maximums.get(color, 0)
+      if (count > maxcolor):
+        maximums[color] = count
+  power = maximums.get('red', 0) * maximums.get('blue', 0) * maximums.get('green', 0)
+  sum += power
+answer = print(sum)
